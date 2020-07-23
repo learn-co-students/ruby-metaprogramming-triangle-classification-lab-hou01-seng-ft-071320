861766fcd1cd6f0ff6b0a5a832b456b62461f375
JuanToanXoup <Johndoan42@gmail.com>
2020-07-23 11:54:01 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,26 +11,18 @@ class Triangle
   def kind
     #returns its type as a symbol. :equilateral, :isosceles, :scalene
     if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 ||
-      @side1 + @side2 <= @side3
-      begin
-        raise TriangleError
-      end
-    elsif @side1 + @side3 <= @side2
-      begin
-        raise TriangleError
-      end
-    elsif @side2 + @side3 <= @side1
+      @side1 + @side2 <= @side3 ||
+      @side1 + @side3 <= @side2 ||
+      @side2 + @side3 <= @side1
       begin
         raise TriangleError
       end
     else
       if @side1 == @side2 && @side2 == @side3 
         self.kind = :equilateral
-      elsif @side1 == @side2 && @side2 != @side3
-        self.kind = :isosceles
-      elsif @side1 == @side3 && @side2 != @side3
-        self.kind = :isosceles
-      elsif @side2 == @side3 && @side2 != @side1
+      elsif @side1 == @side2 && @side2 != @side3 ||
+            @side1 == @side3 && @side2 != @side3 ||
+            @side2 == @side3 && @side2 != @side1
         self.kind = :isosceles
       elsif @side1 != @side2 && @side1 != @side3
         self.kind = :scalene
