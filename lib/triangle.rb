32aadda1a17cd2afd7c223789ce7636454faacb7
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-07-23 13:43:38 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,8 +8,17 @@ class Triangle
   end
 
   def kind
+    if l1 <= 0 || l1 >= l2 + l3 || l2 >= l1 + l3 || l3 >= (l1 + l2)
+      raise TriangleError
+    elsif l1 == l2 && l2 == l3
+      :equilateral
+    elsif l1 == l2 || l2 == l3 || l1 == l3
+      :isosceles
+    else
+      :scalene
+    end
   end
-  
+
   class TriangleError < StandardError
   end
 end
